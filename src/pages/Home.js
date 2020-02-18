06921061a858a2e89291a97dae217d60185b291b
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-02-18 01:07:11 +0100

    update app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,8 @@ export default function Home () {
   return (
     <div className="jumbutron">
       <Link to='/editor'>
-        <h1>PICODE <i className="fas fa-play"></i></h1>
-        <h4 className="p-0 m-0">Write and run code</h4>
+        <h1>PICO<i className="fas fa-play"></i>E</h1>
+        <h4 className="p-0 m-0">Write and run some code</h4>
       </Link>
       
       <p>
