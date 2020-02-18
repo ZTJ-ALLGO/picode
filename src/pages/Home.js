4e4aeca2d0accded4cf4a3305d20c74d725fda4b
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-02-18 01:00:37 +0100

    update app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import codeImg from '../img/code.png';
+import '../styles/home.css';
 
 export default function Home () {
   return (
     <div className="jumbutron">
       <Link to='/editor'>
-        <h1><span className="bg-rose">PI</span>CODE</h1>        
+        <h1>PICODE <i className="fas fa-play"></i></h1>
+        <h4 className="p-0 m-0">Write and run code</h4>
       </Link>
-
-      <div style={{ width: '50%' }}>
-        <img src={codeImg} alt="code" className="w-100" />
-      </div>
+      
+      <p>
+        Created with <i className="fas fa-coffee"></i> and
+        <i className="fas fa-heart ml-5"></i> by <a href="https://github.com/haikelfazzani">Haikel Fazzani</a>
+      </p>
     </div>
   )
 }
